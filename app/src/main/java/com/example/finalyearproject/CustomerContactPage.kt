529f09fdaf16c88939fb3a5ecdf86db71916329c
Ujwal710 <94924733+Ujwal710@users.noreply.github.com>
2023-02-15 14:34:51 +0000

    customerAccount(Contact)page

---
+++ app/src/main/java/com/example/finalyearproject/CustomerContactPage.kt
@@ -0,0 +1,12 @@
+package com.example.finalyearproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalyearproject.R
+
+class CustomerContactPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_contact_page)
+    }
+}
