cd32075180b27b9e217dbd8ebc002e291cf43450
Ujwal <p2603348@my365.dmu.ac.uk>
2023-02-16 22:57:36 +0000

    completed the layout for the account page on customer side

--- app/src/main/java/com/example/finalyearproject/CustomerContactPage.kt
+++ app/src/main/java/com/example/finalyearproject/CustomerContactPage.kt
@@ -1,12 +1,45 @@
 package com.example.finalyearproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.finalyearproject.R
 
 class CustomerContactPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_contact_page)
+    }
+
+    fun navHome(view: View) {
+        val intent = Intent(this, RegisterDiffActivity::class.java)
+        startActivity(intent)
+
+
+    }
+    fun navShop(view: View) {
+        val intent = Intent(this, RegisterDiffActivity::class.java)
+        startActivity(intent)
+
+
+    }
+    fun navCart(view: View) {
+        val intent = Intent(this, RegisterDiffActivity::class.java)
+        startActivity(intent)
+
+
+    }
+    fun navWishlist(view: View) {
+        val intent = Intent(this, RegisterDiffActivity::class.java)
+        startActivity(intent)
+
+
+    }
+    fun navAccount(view: View) {
+        val intent = Intent(this, CustomerContactPage::class.java)
+        startActivity(intent)
+
+
     }
 }
